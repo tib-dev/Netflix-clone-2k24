86923ab98004c389b4334c126d3b35312f7d0f01
amiweb-7 <132199829+tib-dev@users.noreply.github.com>
2024-08-20 13:01:57 +0300

    Update Home

--- Netflix-clone/src/Pages/Home.jsx
+++ Netflix-clone/src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 import Banner from "../Components/Banner/Banner";
-import RowList from "../components/Main/RowList/RowList";
+import RowList from "../Components/Main/RowList/RowList";
 
 const Home = () => {
   return (
